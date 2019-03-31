8b0f57634e6bf28b6f54dde1756919e843159903
Przemyslaw Chmiel <dev@PrzemyslawChmiel.com>
2019-03-31 23:41:14 +0100

    finished script for game

--- app.js
+++ app.js
@@ -2,20 +2,20 @@ const PLAYER1 = 'fa-circle-o';
 const PLAYER2 = 'fa-times';
 let round = 1;
 const board = [
-    ["", "", ""],
-    ["", "", ""],
-    ["", "", ""]
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', '']
 ];
 
 const combinations = [
-    ['0', '1', '2'],
-    ['3', '4', '5'],
-    ['6', '7', '8'],
-    ['0', '3', '6'],
-    ['1', '4', '7'],
-    ['2', '5', '8'],
-    ['0', '4', '8'],
-    ['2', '4', '6']
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
 ]
 
 const boxes = [...document.querySelectorAll('.box')];
@@ -29,12 +29,27 @@ function pick(event) {
     const turn = round % 2 === 0 ? PLAYER2 : PLAYER1;
     if (board[row][column] !== '') return;
     event.target.classList.add(turn);
+    board[row][column] = turn;
     round++;
 
-    check();
+    console.log(check());
 }
 
 function check() {
     const result = board.reduce((total, row) => total.concat(row));
-    console.log(result);
+    let winner = null;
+    let moves = {
+        'fa-times': [],
+        'fa-circle-o': [],
+    };
+    result.forEach((field, index) => moves[field] ? moves[field].push(index) : null);
+    combinations.forEach(combination => {
+        if (combination.every(index => moves[PLAYER1].indexOf(index) > -1)) {
+            winner = 'Winner: Player 1';
+        }
+        if (combination.every(index => moves[PLAYER2].indexOf(index) > -1)) {
+            winner = 'Winner: Player 2';
+        }
+    });
+    return winner;
 }
